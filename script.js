f03b74d45d7d8b477f0f9c2414c62c619b46ce1b
MotaharHossainMohim <motaharhossainmohim@gmail.com>
2025-10-21 01:38:34 +0600

    Complite heart icon  click  and edit some design

---
+++ script.js
@@ -0,0 +1,18 @@
+// function for
+function getInnertext(id) {
+  const htmlElement = document.getElementById(id);
+  const htmlElementText = htmlElement.innerText;
+  const htmlElementTextConverted = parseInt(htmlElementText);
+
+  return htmlElementTextConverted;
+}
+
+// heart icon
+const hearts = document.getElementsByClassName("heart-click");
+
+for (let heart of hearts) {
+  heart.addEventListener("click", function () {
+    const countHeart = document.getElementById("heart-click-count");
+    countHeart.innerText = getInnertext("heart-click-count") + 1;
+  });
+}
